485e4046ad78f75fa04d3e75e91d155bc0dd045d
Falekin7 <falekinldg@gmail.com>
2024-06-26 15:32:40 -0300

    router pages

---
+++ cvInterApp/src/application/pages/About.tsx
@@ -0,0 +1,5 @@
+const About: React.FC = () => {
+  return <div>About page</div>;
+};
+
+export default About;
---
+++ cvInterApp/src/application/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+const NotFound: React.FC = () => {
+  return <div>Not found page</div>;
+};
+
+export default NotFound;
