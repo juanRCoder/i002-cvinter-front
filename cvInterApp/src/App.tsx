d5c25852c582ff27aaf31ac6fc3a2d61fb906a69
juanRCoder <guillecarck@gmail.com>
2024-06-28 11:57:13 -0500

    feat: implement aboute view

--- cvInterApp/src/App.tsx
+++ cvInterApp/src/App.tsx
@@ -4,14 +4,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />;
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-
-  );
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App;
--- cvInterApp/src/application/dashboard/components/Footer.tsx
+++ cvInterApp/src/application/dashboard/components/Footer.tsx
@@ -1,20 +1,20 @@
-import cvinter_logo1 from '../../../assets/cvinter_logo_1.png';
+import cvinter_white from '../../../assets/Cvinter_white.png';
 
 const Footer = () => {
   return (
-    <footer className='bg-musgo font-lato pt-5 px-16 pb-4'>
+    <footer className='bg-musgo font-lato pt-10 px-16 pb-4'>
       <section className='flex justify-between mx-7'>
-        <div className='h-32 w-32'>
+        <div className='h-24 w-24'>
           <img
             className='w-full h-full object-cover'
-            src={cvinter_logo1}
+            src={cvinter_white}
             title='cvinter_logo'
             alt='cvinter_logo'
           />
         </div>
         <div className='text-white mt-12'>
-          <h1 className='text-right font-bold text-xl mb-2'>Links</h1>
-          <ul className='text-right flex flex-col gap-5'>
+          <h1 className='text-right font-bold text-lg mb-3'>Links</h1>
+          <ul className='text-right flex flex-col gap-4'>
             <li className='hover:font-semibold'><a href="/">Sobre nosotros</a></li>
             <li className='hover:font-semibold'><a href="/">Politicas de privacidad</a></li>
             <li className='hover:font-semibold'><a href="/">Cookies</a></li>
