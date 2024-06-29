a1b761e06f4f0b5c5ea2675aab928b60bb170b11
Falekin7 <falekinldg@gmail.com>
2024-06-29 18:17:52 -0300

    actualizacion develop completa

--- cvInterApp/src/application/routes/routes.tsx
+++ cvInterApp/src/application/routes/routes.tsx
@@ -2,7 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import About from "../pages/About";
-import Contact from "../pages/Contact";
+// import Contact from "../pages/Contact";
+import Register from "../auth/components/Register";
+import Login from "../auth/components/Login";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +19,14 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />
   },
-
+  {
+    path: "/about",
+    element: <About />
+  },
+  // {
+  //   path: "/contact",
+  //   element: <Contact />
+  // }
   {
     path: "*",
     element: <NotFound />,
