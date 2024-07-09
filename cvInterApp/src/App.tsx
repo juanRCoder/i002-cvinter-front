99ff592399efc0cebabca918057f349c602afa87
jesus <veraneriquej98@gmail.com>
2024-07-08 21:39:38 -0300

    Cambios en el NavBar, contact  y ajuste resposive

--- cvInterApp/src/App.tsx
+++ cvInterApp/src/App.tsx
@@ -1,8 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./application/dashboard/components/Footer";
+import NavBar from "./application/pages/NavBar";
 
 function App() {
-  return <Outlet />
-
+  return <>
+  <NavBar/>
+  <Outlet />
+  <Footer />
+</>
 }
 
 export default App;
--- cvInterApp/src/application/pages/Home.tsx
+++ cvInterApp/src/application/pages/Home.tsx
@@ -1,10 +1,5 @@
-import Footer from "../dashboard/components/Footer";
 import Hero from "./Hero";
-import Features from "./Features";
-import NavBar from "./NavBar";
-import Product from "./Product";
 import HowItWorks from "./HowItWorks";
-import NotFound from "./NotFound";
 import Contact from "./Contact";
 
 
@@ -12,14 +7,12 @@ import Contact from "./Contact";
 const Home = () => {
   return (
     <div>
-      <NavBar />
+     
       <Hero />
-      <Features />
-      <Product />
       <HowItWorks />
       <Contact />
-      <Footer />
-      <NotFound />
+     
+     
 
     </div>
   );
